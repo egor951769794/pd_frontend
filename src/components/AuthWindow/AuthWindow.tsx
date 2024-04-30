bd85029f298a53cab7dc929116002bb6f01f335f
Egor <egor951769794@gmail.com>
2024-04-30 22:53:19 +0600

    отправка данных с окна авторизации на сервер

--- src/components/AuthWindow/AuthWindow.tsx
+++ src/components/AuthWindow/AuthWindow.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './AuthWindow.css';
 
+import axios from 'axios';
+
 
 type AuthWindowProps = {
   
@@ -9,21 +11,60 @@ type AuthWindowProps = {
 
 export default function AuthWindow(props: AuthWindowProps) {
 
+  const BACKEND_BASE_URL = 'http://82.97.252.126:8008'
+
   const [windowState, setWindowState] = useState(0);
+
+  const [registrationData, setRegistrationData] = useState(
+    {
+      username: '',
+      password: '',
+    }
+  )
+
+  const [authData, setAuthData] = useState(
+    {
+      username: '',
+      password: '',
+    }
+  )
+
+
+  const handleReg = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRegistrationData({...registrationData, [event.target.name] : event.target.value})
+  }
+
+  const handleAuth = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAuthData({...authData, [event.target.name] : event.target.value})
+  }
+
+  const handleRegSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    axios.post(BACKEND_BASE_URL.concat("/auth"), registrationData)
+    .then((respone) => console.log(respone))
+    .catch((error) => console.log(error))
+  }
+
+  const handleAuthSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    axios.post(BACKEND_BASE_URL.concat("/auth/signin"), authData)
+    .then((respone) => console.log(respone))
+    .catch((error) => console.log(error))
+  }
 
   return (
     <div className='auth-container'>
       {windowState ?
-      <form className='auth-form'>
-      <input name='email' type='text' placeholder='Адрес эл.почты' />
-      <input name='password' type='password' placeholder='Пароль' />
-      <button type='submit'>Зарегистрироваться</button>
+      <form className='auth-form' onSubmit={(event) => handleRegSubmit(event)}>
+      <input name='username' type='text' placeholder='Адрес эл.почты' value={registrationData.username} onChange={(event) => handleReg(event)}/>
+      <input name='password' type='password' placeholder='Пароль' value={registrationData.password} onChange={(event) => handleReg(event)}/>
+      <button type='submit' className='auth-button'>Зарегистрироваться</button>
       </form>
       : 
-      <form className='auth-form'>
-        <input name='email' type='text' placeholder='Адрес эл.почты' />
-        <input name='password' type='password' placeholder='Пароль' />
-        <button type='submit'>Войти</button>
+      <form className='auth-form' onSubmit={(event) => handleAuthSubmit(event)}>
+        <input name='username' type='text' placeholder='Адрес эл.почты' value={authData.username} onChange={(event) => handleAuth(event)}/>
+        <input name='password' type='password' placeholder='Пароль' value={authData.password} onChange={(event) => handleAuth(event)}/>
+        <button type='submit' className='auth-button'>Войти</button>
       </form>}
       <div className='auth-switch'>
         <div className={'auth-switch-option'.concat(windowState? '' : ' selected')} onClick={() => setWindowState(0)}>Вход</div>
